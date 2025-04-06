ce7e640724df5b1c803287fdb213701812aa66bb
Freddy Morales <freddy.morales@usil.pe>
2025-04-06 13:36:35 -0500

    class validator in create reservation dto

--- apps/reservations/src/dto/create-reservation.dto.ts
+++ apps/reservations/src/dto/create-reservation.dto.ts
@@ -1,6 +1,20 @@
+import { Type } from 'class-transformer';
+import { IsDate, IsNotEmpty, IsString } from 'class-validator';
+
 export class CreateReservationDto {
+  @IsDate()
+  @Type(() => Date)
   startDate: Date; // Start date of the reservation
+
+  @IsDate()
+  @Type(() => Date)
   endDate: Date; // End date of the reservation
+
+  @IsString()
+  @IsNotEmpty()
   placeId: string; // ID of the place being reserved
+
+  @IsString()
+  @IsNotEmpty()
   invoiceId: string; // ID of the invoice associated with the reservation
 }
