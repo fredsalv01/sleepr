8eec445cbb0ebe9dfd99bbcd2987520addaa4894
Freddy Morales <freddy.morales@usil.pe>
2025-04-07 23:05:22 -0500

    jwt authentication complete

--- apps/auth/src/main.ts
+++ apps/auth/src/main.ts
@@ -2,9 +2,10 @@ import { NestFactory } from '@nestjs/core';
 import { AuthModule } from './auth.module';
 import { Logger } from 'nestjs-pino';
 import { ConfigService } from '@nestjs/config';
-
+import * as cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AuthModule);
+  app.use(cookieParser());
   app.useLogger(app.get(Logger));
   console.log('Starting auth service...');
   const configService = app.get(ConfigService);
