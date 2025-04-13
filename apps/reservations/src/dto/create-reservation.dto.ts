20e5aeea394b96d416bcce475ab9e418435a39e1
Freddy Morales <freddy.morales@usil.pe>
2025-04-12 21:28:01 -0500

    reservation conected with payment in stripe api

--- apps/reservations/src/dto/create-reservation.dto.ts
+++ apps/reservations/src/dto/create-reservation.dto.ts
@@ -1,6 +1,11 @@
 import { Type } from 'class-transformer';
-import { IsDate, IsDefined, IsNotEmpty, IsNumber, IsString, ValidateNested } from 'class-validator';
-import { CardDto } from './card.dto';
+import {
+  IsDate,
+  IsDefined,
+  IsNotEmptyObject,
+  ValidateNested,
+} from 'class-validator';
+import { CreateChargeDto } from '@app/common';
 
 export class CreateReservationDto {
   @IsDate()
@@ -11,19 +16,9 @@ export class CreateReservationDto {
   @Type(() => Date)
   endDate: Date; // End date of the reservation
 
-  @IsString()
-  @IsNotEmpty()
-  placeId: string; // ID of the place being reserved
-
-  @IsString()
-  @IsNotEmpty()
-  invoiceId: string; // ID of the invoice associated with the reservation
-
   @IsDefined()
-  @IsNotEmpty()
+  @IsNotEmptyObject()
   @ValidateNested()
-  card: CardDto;
-
-  @IsNumber()
-  amount: number;
+  @Type(() => CreateChargeDto)
+  charge: CreateChargeDto;
 }
--- apps/reservations/src/reservations.service.ts
+++ apps/reservations/src/reservations.service.ts
@@ -4,20 +4,35 @@ import { UpdateReservationDto } from './dto/update-reservation.dto';
 import { ReservationsRepository } from './reservations.repository';
 import { PAYMENT_SERVICE } from '@app/common';
 import { ClientProxy } from '@nestjs/microservices';
+import { map } from 'rxjs';
 
 @Injectable()
 export class ReservationsService {
   constructor(
     private readonly reservationsRepository: ReservationsRepository,
-    @Inject(PAYMENT_SERVICE) paymentsService: ClientProxy
+    @Inject(PAYMENT_SERVICE) private readonly paymentsService: ClientProxy,
   ) {}
 
   async create(createReservationDto: CreateReservationDto, userId: string) {
-    return this.reservationsRepository.create({
-      ...createReservationDto,
-      timestamp: new Date(),
-      userId,
-    });
+    return this.paymentsService
+      .send('create_charge', createReservationDto.charge)
+      .pipe(
+        map((response) => {
+          console.log(
+            '🚀 ~ ReservationsService ~ .subscribe ~ response:',
+            response,
+          );
+          if (!response) {
+            throw new Error('Error creating charge');
+          }
+          return this.reservationsRepository.create({
+            ...createReservationDto,
+            timestamp: new Date(),
+            userId,
+            invoiceId: response.id,
+          });
+        }),
+      );
   }
 
   async findAll() {
