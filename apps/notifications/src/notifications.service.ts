135db13585f7d111c77716fb09898f1ac4ec93b7
Freddy Morales <freddy.morales@usil.pe>
2025-04-17 20:00:12 -0500

    notify payment by email complete

---
+++ apps/notifications/src/notifications.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@nestjs/common';
+import { NotifyEmailDto } from './dto/notify-email.dto';
+import * as nodemailer from 'nodemailer';
+import { ConfigService } from '@nestjs/config';
+
+@Injectable()
+export class NotificationsService {
+  constructor(private readonly configService: ConfigService) {}
+  private readonly transporter = nodemailer.createTransport({
+    port: 465,
+    host: 'smtp.gmail.com',
+    secure: true,
+    auth: {
+      type: 'OAuth2',
+      user: this.configService.get('SMTP_USER'),
+      clientId: this.configService.get('GOOGLE_OAUTH_CLIENT_ID'),
+      clientSecret: this.configService.get('GOOGLE_OAUTH_CLIENT_SECRET'),
+      refreshToken: this.configService.get('GOOGLE_OAUTH_REFRESH_TOKEN'),
+      accessToken: this.configService.get('GOOGLE_OAUTH_ACCESS_TOKEN'),
+    },
+  });
+  async notifyEmail({ email, text }: NotifyEmailDto) {
+    await this.transporter.sendMail({
+      from: this.configService.get('SMTP_USER'),
+      to: email,
+      subject: 'Sleepr Notification',
+      text: text,
+    });
+    console.log('se mando', email);
+  }
+}
--- apps/payments/src/payments.service.ts
+++ apps/payments/src/payments.service.ts
@@ -1,7 +1,9 @@
-import { CreateChargeDto } from '@app/common';
-import { Injectable } from '@nestjs/common';
+import { CreateChargeDto, NOTIFICATIONS_SERVICE } from '@app/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { ClientProxy } from '@nestjs/microservices';
 import Stripe from 'stripe';
+import { PaymentsCreateChargeDto } from './dto/payments-create-charge.dto';
 
 @Injectable()
 export class PaymentsService {
@@ -11,15 +13,31 @@ export class PaymentsService {
       apiVersion: '2025-03-31.basil',
     },
   );
-  constructor(private readonly configService: ConfigService) {}
+  constructor(
+    private readonly configService: ConfigService,
+    @Inject(NOTIFICATIONS_SERVICE)
+    private readonly notificationsService: ClientProxy, // Replace with the actual type of your notifications client)
+  ) {}
 
-  async createCharge({ amount }: CreateChargeDto) {
+  async createCharge({ amount, email }: PaymentsCreateChargeDto) {
     const paymentIntent = await this.stripe.paymentIntents.create({
       amount: amount * 100,
       confirm: true,
       currency: 'usd',
       payment_method: 'pm_card_visa',
+      receipt_email: email,
+      description: 'Payment for reservation',
+      automatic_payment_methods: {
+        enabled: true,
+        allow_redirects: 'never',
+      },
     });
+
+    this.notificationsService.emit('notify_email', {
+      email,
+      text: `Your payment of $${amount} has completed successfully`,
+    });
+
     console.log(
       '🚀 ~ PaymentsService ~ createCharge ~ paymentIntent:',
       paymentIntent,
